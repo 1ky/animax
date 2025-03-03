1eb0180543d694728799acdbab15e15db1f5e07f
Kyle Shepherd <kyshepherd@protonmail.com>
2025-03-03 15:42:19 -0400

    Implement routing path to anime detail page

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -1,12 +1,16 @@
 import { createBrowserRouter } from "react-router-dom";
 import Layout from "./pages/Layout";
 import Homepage from "./components/Homepage";
+import AnimeDetailPage from "./pages/AnimeDetailPage";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Layout />,
-    children: [{ index: true, element: <Homepage /> }],
+    children: [
+      { index: true, element: <Homepage /> },
+      { path: "/anime/:id", element: <AnimeDetailPage /> },
+    ],
   },
 ]);
 
--- src/components/Homepage.tsx
+++ src/components/Homepage.tsx
@@ -3,6 +3,7 @@ import { graphql } from "../gql";
 import { PageQuery } from "../gql/graphql";
 import AnimeCard from "./AnimeCard";
 import Carousel from "./Carousel";
+import { useNavigate } from "react-router-dom";
 
 const animePageQuery = graphql(`
   query Page(
@@ -33,6 +34,8 @@ const animePageQuery = graphql(`
 `);
 
 const Homepage = () => {
+  const navigate = useNavigate();
+
   const [{ data, fetching, error }] = useQuery<PageQuery>({
     query: animePageQuery,
     variables: {
@@ -51,9 +54,13 @@ const Homepage = () => {
       <div className="flex justify-center">
         <Carousel />
       </div>
-      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-2 p-6 w-full min-w-[300px]">
+      <div className="grid grid-cols-2 sm:grid-cols-4 md:grid-cols-6 gap-2 p-6 w-full min-w-[300px] cursor-pointer">
         {data?.Page?.media?.map((anime) => (
-          <div key={anime?.id} className="">
+          <div
+            key={anime?.id}
+            className=""
+            onClick={() => navigate(`/anime/${anime?.id}`)}
+          >
             <AnimeCard
               image={anime?.coverImage?.large || ""}
               title={anime?.title?.english || anime?.title?.romaji || ""}
